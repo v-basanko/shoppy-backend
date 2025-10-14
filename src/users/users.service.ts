dfa177a12414632433a1398c1fb1214ab230d31a
Viktor Basanko <v.basanko@outlook.com>
2025-10-15 02:15:34 +0300

    feature/SHPBE-04: Create User and Prisma

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { CreateUserRequestDto } from './dto/create-user.request.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  createUser(data: CreateUserRequestDto) {
-    console.log(data);
-    throw new Error('Method not implemented.');
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: CreateUserRequestDto): Promise<{ id: string; email: string }> {
+    try {
+      const hashedPassword: string = await bcrypt.hash(data.password, 10);
+      return this.prismaService.user.create({
+        data: {
+          ...data,
+          password: hashedPassword,
+        },
+        select: {
+          id: true,
+          email: true,
+        },
+      });
+      // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents,@typescript-eslint/no-explicit-any
+    } catch (err: Error | any) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (err.code === 'P2002') {
+        throw new UnprocessableEntityException('User with this email already exists');
+      }
+      throw err;
+    }
   }
 }
